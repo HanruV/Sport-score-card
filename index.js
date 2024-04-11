b81c771ee664a7a0e548b8d7d4f5743f324e36c0
Hanru Vorster <hanru.vorster@gmail.com>
2024-04-11 11:00:41 +0200

    added js functionality to buttons and have them display the increments in the correct windows

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
 let homeCount = document.getElementById("hcount")
 
-console.log(homeCount)
+// home count
+let hcount = 0
 
+function increaseHomeScoreOne() {
+    hcount += 1
+    homeCount.textContent = hcount
+}
+
+function increaseHomeScoreTwo() {
+    hcount += 2
+    homeCount.textContent = hcount
+}
+
+function increaseHomeScoreThree() {
+    hcount += 3
+    homeCount.textContent = hcount
+}
+
+// guest count
+
+let guestCount = document.getElementById("gcount")
+
+let gcount = 0
+
+function increaseGuestScoreOne() {
+    gcount += 1
+    guestCount.textContent = gcount
+}
+
+function increaseGuestScoreTwo() {
+    gcount += 2
+    guestCount.textContent = gcount
+}
+
+function increaseGuestScoreThree() {
+    gcount += 3
+    guestCount.textContent = gcount
+}
